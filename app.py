b11a4f921981b9ccb4f0d17befe0fd3842a08815
Hanna Zamora <hannazamora1102@gmail.com>
2019-08-20 14:21:25 -0700

    commit finproj

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+from tinydb import TinyDB, Query
+import googlemaps
+gmaps = googlemaps.Client(key= "AIzaSyDUHam462KHkSSRNdEfFCtPBeQL1FQ7mG4")
+db = TinyDB('db/db.json')
+parks_db = TinyDB("db/parks.json")
+# {"_default": {"1": {"type": "name"}}}
+# for item in db:
+#     print(item)
+
+app = Flask(__name__)
+def create_id():
+    return len(db.all())+1
+
+@app.route("/")
+def index():
+    return render_template("Home Page.html")
+
+@app.route("/lost_found") ## telling the URL of the project, connect button to that link
+def lost_found():
+    pets = db.all()
+    return render_template("lstnfnd.html", pets=pets)
+
+@app.route("/lost_pet/", methods=["GET", "POST"])
+def lost_pet():
+    if request.method == 'POST':
+        # collect information from request.form (using the names
+        # attributes from your inputs) and create a database entry;
+        # then google "URL redirect"
+
+        db.insert({
+            "id":create_id(),
+            "name":request.form["name"],
+            "address":request.form["address"],
+            "info":request.form["info"],
+            "contact":request.form["contact"],
+            "date":request.form["date"],
+            "coord":gmaps.geocode(request.form["address"])[0]["geometry"]["location"]
+        })
+        return redirect(url_for("lost_found"))
+    return render_template("lostpet.html")
+
+@app.route("/lost_pet/<int:pet_id>")
+def pet_info(pet_id):
+    Pet = Query()
+    pet = db.search(Pet.id == pet_id)[0]
+    return render_template("pet_info.html", pet=pet)
+
+# @app.route("/petlost/<string:petname>/")
+# def pet_info():
+#    return render_template("Lost pet information")
+
+# @app.route("/petfound/<string:petname>/")
+# def found_pet():
+#    return render_template("Yay you found the pet")
+
+@app.route("/parks_beaches")
+def parks_beaches():
+    parks=parks_db.all()
+    return render_template("parks_beaches.html", parks=parks)
+
+@app.route("/about_us")
+def about_us():
+    return render_template("aboutus.html")
+
+if __name__ == "__main__":
+    app.run()
---
+++ parks.py
@@ -0,0 +1,64 @@
+from app import parks_db, gmaps
+parks_beaches = [
+    {
+        "name": "Marymoor Park",
+        "address": "6046 West Lake Sammamish Pkwy NE, Redmond, WA 98052",
+    },
+    {
+        "name": "Jasper's Dog Park",
+        "address": "11225 NE 120th St, Kirkland, WA 98034",
+    },
+    {
+        "name": "Grass Lawn Park",
+        "address": "7031 148th Ave NE, Redmond, WA 98052",
+    },
+    {
+        "name": "Mark Twain Park",
+        "address": "10625 132nd Ave NE, Kirkland, WA 98033",
+    },
+    {
+        "name": "Sixty Acres Park",
+        "address": "15200 NE 116th St, Redmond, WA 98052",
+    },
+    {
+        "name": "Hartman Park",
+        "address": "17300 NE 104th St, Redmond, WA 98052",
+    },
+    {
+        "name": "Bear Creek Park",
+        "address": "Evans Creek Trail, Redmond, WA 98053",
+    },
+    {
+        "name": "Bridle Trails State Park",
+        "address": "5300 116th Ave NE, Kirkland, WA 98033",
+    },
+    {
+        "name": "Richmond Beach Saltwater Park",
+        "address": "2021 NW 190th St, Shoreline, WA 98177",
+    },
+    {
+        "name": "Luther Burbank Park",
+        "address": "2040 84th Ave SE, Mercer Island, WA 98040",
+    },
+    {
+        "name": "Idylwood Park",
+        "address": "3650 West Lake Sammamish Pkwy NE, Redmond, WA 98052",
+    },
+    {
+        "name": "Tambark Creek Park",
+        "address": "17217 35th Ave SE, Bothell, WA 98012",
+    },
+    {
+        "name": "Warren G. Magnuson Park Off Leash Area",
+        "address": "7400 Sand Point Way NE, Seattle, WA 98115",
+    },
+
+
+]
+for park in parks_beaches:
+    parks_db.insert({
+        "name": park["name"],
+        "address": park["address"],
+        "coord": gmaps.geocode(park["address"])[0]["geometry"]["location"]
+
+    })
